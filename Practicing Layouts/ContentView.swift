6f32f217c037c3506abc007e67e2adc237106960
sinelanguage@gmail.com <sinelanguage@gmail.com>
2020-05-09 15:53:59 -0400

    Initial Commit

---
+++ Practicing Layouts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Practicing Layouts
+//
+//  Created by Adam Winick on 2020-05-09.
+//  Copyright © 2020 Adam Winick. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
